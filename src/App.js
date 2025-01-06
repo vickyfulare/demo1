4561f9a69ae5950d60ac081114ef8607376ed4e1
Vicky <vickyfulare1998@gmail.com>
2025-01-06 19:49:50 +0530

    first comment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To My First React Application</h1>
+    </div>
+  );
+}
+
+export default App;
